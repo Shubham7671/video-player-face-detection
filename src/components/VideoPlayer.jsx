3497151ccda8333ee7f138c185e080656cee47b9
Shubham7671 <sk767196@gmail.com>
2024-01-06 17:04:36 +0530

    Adding canva and uploading functionality

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,6 +1,33 @@
 import React from 'react'
+import { fabric } from 'fabric';
+import * as faceapi from 'face-api.js';
+import { useRef,useState,useEffect } from 'react';
 
 export default function VideoPlayer() {
+    const videoRef = useRef(null);
+    const canvasRef = useRef(null);
+  
+  
+    const handlePlayPause = () => {
+      const video = videoRef.current;
+      setIsPlaying(!isPlaying);
+  
+      if (isPlaying) {
+        video.pause();
+      } else {
+        video.play();
+      }
+    };
+  
+    const handleVideoUpload = (e) => {
+      const video = videoRef.current;
+      const file = e.target.files[0];
+  
+      if (file) {
+        const blobURL = URL.createObjectURL(file);
+        video.src = blobURL;
+      }
+    };
     return (
         <>
             <div className="heading" >
