7ef9b9c3c01ad5b87ab2df1f4ff2bc3f7f9c256d
Shubham7671 <sk767196@gmail.com>
2024-01-06 18:29:59 +0530

    autoplay functionality

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -11,7 +11,70 @@ export default function VideoPlayer() {
     const [isPlaying, setIsPlaying] = useState(false);
     const [modelsLoaded, setModelsLoaded] = useState(false);
 
-   
+    useEffect(() => {
+        const loadModels = async () => {
+            try {
+                await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+                setModelsLoaded(true);
+            } catch (error) {
+                console.error('Error loading models:', error);
+            }
+        };
+        loadModels();
+    }, []);
+
+    useEffect(() => {
+        if (modelsLoaded) {
+            const canvas = new fabric.Canvas(canvasRef.current);
+            const displaySize = { width: videoRef.current.width, height: videoRef.current.height };
+
+            const drawFaces = async () => {
+                try {
+                    if (videoRef.current.ended) {
+                        setIsPlaying(false);
+                    }
+                    if (videoRef.current.paused || videoRef.current.ended) {
+                        canvas.clear();
+                        return;
+                    }
+                    canvas.clear();
+
+                    const detections = await faceapi.detectAllFaces(
+                        videoRef.current,
+                        new faceapi.TinyFaceDetectorOptions({ scoreThreshold: 0.3 })
+                    );
+
+                    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+                    resizedDetections.forEach((detection) => {
+                        console.log(detection);
+                        // console.log(detection.box.right);
+                        const rect = new fabric.Rect({
+                            right:detection.box.right,
+                            bottom:detection.box.bottom,
+                            left: detection.box.left,
+                            top: detection.box.top,
+                            width: detection.box.width,
+                            height: detection.box.height,
+                            fill: 'transparent',
+                            strokeWidth: 1,
+                            stroke: 'green',
+                        });
+                        canvas.add(rect);
+                    });
+                } catch (error) {
+                    console.error('Error drawing faces:', error);
+                }
+            };
+
+            intervalIdRef.current = setInterval(drawFaces, 400);
+
+            return () => {
+                clearInterval(intervalIdRef.current);
+                canvas.dispose();
+            };
+        }
+    }, [modelsLoaded]);
 
     const handlePlayPause = () => {
         const video = videoRef.current;
@@ -24,7 +87,7 @@ export default function VideoPlayer() {
         }
     };
 
-
+    
 
     const handleVideoUpload = (e) => {
         const video = videoRef.current;
@@ -33,6 +96,7 @@ export default function VideoPlayer() {
         if (file) {
             const blobURL = URL.createObjectURL(file);
             video.src = blobURL;
+            setIsPlaying(true)
         }
     };
     return (
@@ -51,7 +115,7 @@ export default function VideoPlayer() {
 
             <div className='container'>
                 <div className='video'>
-                    <video ref={videoRef} controls={false} width="350" height="280" />
+                    <video ref={videoRef} controls={false} width="350" height="280" autoPlay />
                 </div>
                 <div className='canva'>
                     <canvas ref={canvasRef} width="350" height="280" />
